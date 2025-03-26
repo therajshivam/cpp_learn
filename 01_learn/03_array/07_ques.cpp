c64ae600dbf928b6159b8e76d3ac8a37bb4a3496
Shivam Raj <rajshivam5984@gmail.com>
2025-03-26 23:36:35 +0530

    swapMinMax

--- 01_learn/03_array/07_ques.cpp
+++ 01_learn/03_array/07_ques.cpp
@@ -4,39 +4,68 @@
 #include <climits>
 using namespace std;
 
-int minNum(int arr[], int size) {
-    int smallest = INT_MAX;
+// int minNum(int arr[], int size) {
+//     int smallest = INT_MAX;
+//     for (int i = 0; i < size; i++)
+//     {
+//         smallest = min(arr[i], smallest);
+//     }
+//     return smallest;
+// }
+
+// int maxNum(int arr[], int size) {
+//     int largest = INT_MIN;
+//     for (int i = 0; i < size; i++)
+//     {
+//         largest = max(arr[i], largest);
+//     }
+//     return largest;
+// }
+
+void swapMinMax(int arr[], int size) {
+
+    int minIdx = -1, maxIdx = -1;
+    int minVal = INT_MAX, maxVal = INT_MIN;
+
     for (int i = 0; i < size; i++)
     {
-        smallest = min(arr[i], smallest);
+        if (arr[i] < minVal) 
+        {
+            minVal = arr[i];
+            minIdx = i;
+        }
+        if (arr[i] > maxVal)
+        {
+            maxVal = arr[i];
+            maxIdx = i;
+        }
     }
-    return smallest;
+
+    if (minIdx != -1 && maxIdx != -1)
+    {
+        swap(arr[minIdx], arr[maxIdx]);
+    }
 }
 
-int maxNum(int arr[], int size) {
-    int largest = INT_MIN;
+void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
     {
-        largest = max(arr[i], largest);
+        cout << arr[i] << " ";
     }
-    return largest;
+    cout << endl;
 }
-
-
 
 int main(){
      int arr[] = {0, 1, 2, 3, 4, 5};
      int size = sizeof(arr)/sizeof(arr[0]);
 
-     cout << "Array is:  ";
-     for (int i = 0; i < size; i++)
-     {
-        cout << arr[i] << " ";
-     }
-     cout << endl;
-     
-     cout << "Smallest Element: " << minNum(arr, size) << endl;
-     cout << "Largest Element: " << maxNum(arr, size) << endl;
+     cout << "Original Array is:  ";
+     printArray(arr, size);
 
+     swapMinMax(arr, size);
+
+     cout << "Array after swapping min and max: ";
+     printArray(arr, size);
+     
     return 0;
 }

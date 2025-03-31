64017e36af2e74a500337eb5612400388fe78af5
Shivam Raj <rajshivam5984@gmail.com>
2025-03-31 18:16:35 +0530

    input

--- 01_learn/04/07.cpp
+++ 01_learn/04/07.cpp
@@ -7,19 +7,30 @@ int main() {
 
     string foods[5];
     int size = sizeof(foods)/sizeof(foods[0]);
+    string temp;
 
     for (int i = 0; i < size; i++)
     {
         cout << "Enter a food you like #" << i+1 << ": ";
-        getline(cin, foods[i]);
+        getline(cin, temp);
+        if (temp == "q")
+        {
+            break;
+        } else
+        {
+            foods[i] = temp;
+        }
+        
+        
     }
 
     cout << "You like the following food items:\n";
 
-    for (string food : foods)
+    for (int i = 0; !foods[i].empty(); i++)
     {
-        cout << food << '\n';
+        cout << i + 1 << ". " << foods[i] << '\n';
     }
     
+
     return 0;
 }

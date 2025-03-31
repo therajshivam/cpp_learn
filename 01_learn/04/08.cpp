72d773ba92f9a53083d4c0f4f3fb28aeb7203240
Shivam Raj <rajshivam5984@gmail.com>
2025-03-31 18:27:36 +0530

    08

---
+++ 01_learn/04/08.cpp
@@ -0,0 +1,28 @@
+// multi dimentional arrays
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int nums[][3] = {{1, 2, 3}, 
+                    {4, 5, 6},
+                    {7, 8, 9}};
+
+    // cout << nums[0][0] << " ";    
+    
+    int rows = sizeof(nums)/sizeof(nums[0]);
+    int columns = sizeof(nums[0])/sizeof(nums[0][0]);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cout << nums[i][j] << " ";
+        }
+        cout << '\n';
+    }
+    
+
+    return 0;
+}

1ca6b32942b3a728c977fb07e52363846ee8ce87
Shivam Raj <rajshivam5984@gmail.com>
2025-03-31 20:53:51 +0530

    09

--- 01_learn/04/09.cpp
+++ 01_learn/04/09.cpp
@@ -8,7 +8,13 @@ using namespace std;
 
 int main(){
 
-    string name = 
+    string name = "Bro";
+    int age = 21;
+    bool student = true;
+
+    cout << &name << '\n';
+    cout << &age << '\n';
+    cout << &student << '\n';
 
     return 0;
 }

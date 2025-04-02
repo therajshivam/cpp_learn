da47f7204dfd553a68105a54a3a4162b22fdc4c9
Shivam Raj <rajshivam5984@gmail.com>
2025-04-02 08:38:58 +0530

    11

--- 01_learn/04/11.cpp
+++ 01_learn/04/11.cpp
@@ -1,2 +1,21 @@
-// 
-// 
+// null pointers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int *pointer = nullptr;
+    int x = 123;
+
+    pointer = &x;
+
+    if (pointer == nullptr)
+    {
+        cout << "address was not assigned\n ";
+    } else
+    {
+        cout << "address was assigned\n";
+        cout << *pointer;
+    }
+}

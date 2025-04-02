0ede18b45358f6a3219a9d38ee8b84e12ed0c31b
Shivam Raj <rajshivam5984@gmail.com>
2025-04-02 08:33:41 +0530

    10

--- 01_learn/04/10.cpp
+++ 01_learn/04/10.cpp
@@ -13,7 +13,18 @@ int main(){
     string name = "Bro";
     string *pName = &name;
 
-    cout << pName << '\n'; // 
+    cout << pName << '\n'; // display pointer
+    cout << *pName << '\n'; // display value at this address
+
+    int age = 21;
+    int *pAge = &age;
+
+    cout << *pAge << '\n';
+
+    string freePizzas[5] = {"Pizza1", "Pizza2", "Pizza3","Pizza4", "Pizza5",};
+    string *pFreePizzas = freePizzas;
+    cout << *pFreePizzas << '\n';
+
 
     return 0;
 }

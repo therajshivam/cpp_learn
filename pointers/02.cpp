eed61e69a967b4910c1364bb395a6ea6c9034cee
Shivam Raj <rajshivam5984@gmail.com>
2025-04-04 23:36:09 +0530

    02

---
+++ pointers/02.cpp
@@ -0,0 +1,21 @@
+// pointers
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int *a = nullptr;
+    int *b = nullptr;
+
+
+    int *c = nullptr;
+    int *d = nullptr;
+
+    int *e = nullptr;
+    int *f = nullptr;
+
+
+
+    return 0;
+}

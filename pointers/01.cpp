8713a8e7b710f5bd5aa7b9204f96d69e3553166d
Shivam Raj <rajshivam5984@gmail.com>
2025-04-05 23:23:43 +0530

    01

--- pointers/01.cpp
+++ pointers/01.cpp
@@ -5,3 +5,28 @@
 // null pointer
 // array and pointers
 // passing pointers to function
+
+
+// SYNTAX : 
+// datatype *pointer_name;
+
+//  &x → Address of x
+//  ptr → Stores address of x
+//  *ptr → Dereferencing (value at address stored in ptr)
+
+// Declaring and Initializing a Pointer
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;   // Normal variable
+    int *ptr = &x; // Pointer storing address of x
+
+    cout << "Value of x: " << x << endl;
+    cout << "Address of x: " << &x << endl;
+    cout << "Pointer ptr stores: " << ptr << endl;
+    cout << "Value at ptr: " << *ptr << endl; // Dereferencing
+
+    return 0;
+}
+

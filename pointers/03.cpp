e656dfbd19d74517a6d20ad4b2f61e5d26d20cf9
Shivam Raj <rajshivam5984@gmail.com>
2025-04-05 23:21:14 +0530

    03

---
+++ pointers/03.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // Array and pointers
+    int a[10];
+    a[0] = 2;
+    a[1] = 4;
+
+    cout << "a: " << a << endl;
+    cout << "&a[0]: " << &a[0] << endl;
+    cout << "*a: " << *a << endl;
+    cout << "a+1: " << a+1 << endl;
+    cout << "&a[1]: " << &a[1] << endl;
+    cout << "*(a+1): " << *(a+1) << endl;
+
+}

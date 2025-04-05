a3b0bafba01b9f81e571b901996e9db1dadb3983
Shivam Raj <rajshivam5984@gmail.com>
2025-04-05 23:13:37 +0530

    02

--- pointers/02.cpp
+++ pointers/02.cpp
@@ -1,19 +1,36 @@
 // pointers
 
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
     
-    int *a = nullptr;
-    int *b = nullptr;
+    // pointers
+    int x = 4;
+    int *p_x = &x;
+    cout << "Addr x: " << &x << endl;
+    cout << "Val p_x: " << p_x << endl;
+    cout << "val *p_x: " << *p_x << endl;
+
+    *p_x = 5;
+    cout << "x: " << x << endl;
+    // pointer addition
+    cout << "Addr p_x: " << p_x << endl;
+    cout << "Addr p_x + 1: " << p_x + 1<< endl;
+    
+    // double pointers
+    int **p_p_x = &p_x;
+    cout << "Addr p_x: " << &p_x << endl;
+    cout << "Val p_p_x: " << p_p_x << endl;
+    cout << "val *p_p_x: " << *p_p_x << endl;
+    cout << "val **p_p_x: " << **p_p_x << endl;
+
+    **p_p_x = 7;
+    cout << "x: " << x << endl;
+    
 
 
-    int *c = nullptr;
-    int *d = nullptr;
 
-    int *e = nullptr;
-    int *f = nullptr;
 
 
 

d155d8e128e69db3ecf0c63fd6576ccf3bdeb209
Shivam Raj <rajshivam5984@gmail.com>
2025-04-25 07:20:40 +0530

    timeComplexity

--- complexity/01_basics.cpp
+++ complexity/01_basics.cpp
@@ -1,20 +1,29 @@
+// Time Complexity :
+// The rate at which the time taken increases wrt the input size
+
+// Rules
+// -> TC to be calculated in worst case scenario
+// -> Avoid constants
+// -> Avoid lower values
+
+// Big-Oh Notation to calculate time complexity -> O(N) ; N -> Time Taken
+
+// Best case / Worst case / Average case
+
 #include<iostream>
 using namespace std;
 
 int main() {
 
-    int i; 
-    i = 20;
+    int n;
     
-
+    for (int i = 0; i < n; i++ ) {
+        for (int j = 0; j < n; j++)
+        {
+            // Block of code
+        }
+        
+    }
 
     return 0;
 }
-
-// new 
-// new
-
-
-
-// new
-// new

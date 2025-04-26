e26d8b122b101ed1863600680576e42552c1f2e5
Shivam Raj <rajshivam5984@gmail.com>
2025-04-26 09:21:33 +0530

    vectors

---
+++ stl/02_vector.cpp
@@ -0,0 +1,32 @@
+// Containers
+// 1. Vectors : dynamic array.
+// It automatically resizes itself when elements are added or removed.
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    vector<int> v; // declaration 
+    // this creates an empty container
+
+    v.push_back(1);
+    v.emplace_back(2);
+
+    // vector of pair data type
+    vector<pair<int, int>> vec;
+
+    vec.push_back({1,2});
+    vec.emplace_back(1,2);
+
+
+
+
+
+
+
+    return 0;
+}
